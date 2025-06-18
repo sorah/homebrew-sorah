42fa3ddc7f48dd8bd519b0e85cdc6bc9aa1935d8
Sorah Fukumori <her@sorah.jp>
2025-06-18 22:43:32 +0900

    mairu
    Closes #1.
    
    Signed-off-by: Sorah Fukumori <her@sorah.jp>

---
+++ Formula/mairu.rb
@@ -0,0 +1,22 @@
+class Mairu < Formula
+  desc "AWS on-memory credentials agent"
+  homepage "https://github.com/sorah/mairu"
+  url "https://github.com/sorah/mairu/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "5e6da78946dd74da63d6bced300e4d2b712740e66280e87aad6ffd957a9854f1"
+  license "Apache-2.0"
+  head "https://github.com/sorah/mairu.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "protobuf" => :build
+  depends_on "rust" => :build
+
+  depends_on "openssl@3"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+end
