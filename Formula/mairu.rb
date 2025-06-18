622f6518334b47f44279b5ace946920f900a57fa
Sorah Fukumori <her@sorah.jp>
2025-06-18 15:44:24 +0000

    mairu: add 0.8.0 bottle.

--- Formula/mairu.rb
+++ Formula/mairu.rb
@@ -11,6 +11,13 @@ class Mairu < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/sorah/sorah"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e73f7b794d24f885ef4a4b6235a819727e025191b04b4a87e49bc2c2517e7f29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "284229a0f332ffe85c70b42f551e6986443968487882f3dcbb7d5d8a5f01086b"
+  end
+
   depends_on "protobuf" => :build
   depends_on "rust" => :build
 
